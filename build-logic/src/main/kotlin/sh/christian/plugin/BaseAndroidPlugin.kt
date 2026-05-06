36ae6922ee5fa21ae2d09cdd084015408cc8ce7c
clover <turtlepaw422@outlook.com>
2026-05-06 00:58:10 -0400

    Fix BaseAndroidPlugin.kt for AGP 9 by using ApplicationExtension and LibraryExtension

--- build-logic/src/main/kotlin/sh/christian/plugin/BaseAndroidPlugin.kt
+++ build-logic/src/main/kotlin/sh/christian/plugin/BaseAndroidPlugin.kt
@@ -1,10 +1,11 @@
 package sh.christian.plugin
 
-import com.android.build.api.dsl.CommonExtension
+import com.android.build.api.dsl.ApplicationExtension
+import com.android.build.api.dsl.LibraryExtension
 import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.getByName
+import org.gradle.kotlin.dsl.configure
 
 @Suppress("unused")
 class BaseAndroidPlugin : Plugin<Project> {
@@ -14,16 +15,33 @@ class BaseAndroidPlugin : Plugin<Project> {
 private fun Project.applyPlugin() {
   plugins.apply("ozone-base")
 
-  extensions.getByName<CommonExtension>("android").apply {
-    compileSdk = 36
+  pluginManager.withPlugin("com.android.application") {
+    extensions.configure<ApplicationExtension>("android") {
+      compileSdk = 36
 
-    defaultConfig {
-      minSdk = 30
+      defaultConfig {
+        minSdk = 30
+      }
+
+      compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+      }
     }
+  }
 
-    compileOptions {
-      sourceCompatibility = JavaVersion.VERSION_17
-      targetCompatibility = JavaVersion.VERSION_17
+  pluginManager.withPlugin("com.android.library") {
+    extensions.configure<LibraryExtension>("android") {
+      compileSdk = 36
+
+      defaultConfig {
+        minSdk = 30
+      }
+
+      compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+      }
     }
   }
 }
